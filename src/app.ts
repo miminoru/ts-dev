a3a1c26043771d3bfc66b419ef57b35aa68f1820
miminoru <miminoru1125@gmail.com>
2023-12-29 15:50:20 +0900

    generic関連の更新

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,33 @@
-console.log("test")
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+// function merge<T,U>(objA:T,objB:U){
+//   return Object.assign(objA,objB)
+// }
+
+const mergedObject = merge({name:'MAX',hobbies:['sports']},{age:30})
+console.log(mergedObject.name)
+console.log(mergedObject.hobbies)
+console.log(mergedObject.age)
+
+type Lengthy = {
+  length : number
+}
+
+function countAndDescribe<T extends Lengthy>(element:T){
+  let descriptionText = '値がありません'
+  if(element.length){
+    descriptionText = '値は' + element.length + '個です'
+  }
+  return [element,descriptionText]
+}
+
+console.log(countAndDescribe(['sports','Cooking']))
+
+
+function extractAndConvert<T extends object, U extends keyof T>(obj:T,key:U){
+  return 'Value: ' + obj[key]
+}
+
+extractAndConvert({name:'Max'}, 'name')
